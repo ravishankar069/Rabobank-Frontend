f8272385134a3a4301bd836130968ae2d42c7a5c
RajamanickamRS <Ravi.Shankar.Rajamanickam@rabobank.nl>
2019-03-21 11:43:56 +0530

    first commit

---
+++ src/app/issue-statement-processor/issue-processor.component.js
@@ -0,0 +1,69 @@
+'use strict';
+
+angular.module('issueStatementProcessor', ['ngFileUpload']).
+    component('issueStatementProcessor', {
+        templateUrl: './app/issue-statement-processor/issue-processor.html',
+        controller: function ($scope,$window) {
+            var customers = new Array();
+            $scope.issue = {
+                count: ''
+            };
+
+            $scope.selectFile = function (file) {
+                $scope.selectedFile = file;
+            };
+
+            $scope.processingIssueStatement = function () {
+                var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+                if (regex.test($scope.selectedFile.name.toLowerCase())) {
+                    if (typeof (FileReader) != "undefined") {
+                        var reader = new FileReader();
+                        reader.onload = function (e) {
+                            var rows = e.target.result.split("\r\n");
+                            for (var i = 0; i < rows.length; i++) {
+                                var cells = rows[i].split(",");
+                                if (cells.length > 1 && i > 0) {
+                                    var customer = {};
+                                    customer.firstName = cells[0];
+                                    customer.surName = cells[1];
+                                    customer.issueCount = cells[2];
+                                    customer.dateOfBirth = cells[3];
+                                    customers.push(customer);
+                                    $scope.$apply(function () {
+                                        $scope.Customers = customers;
+                                        $scope.IsVisible = true;
+                                    });
+                                }
+                            }
+                        }
+                        reader.readAsText($scope.selectedFile);
+                    }
+                } else {
+                    $window.alert("Please upload a valid CSV file.");
+                }
+            }
+
+            $scope.filteringOnIssueCount = function () {
+                var input, filter, table, tr, td, i, txtValue;
+                input = document.getElementById("myInput");
+                filter = input.value.toUpperCase();
+                table = document.getElementById("tblCustomers");
+                tr = table.getElementsByTagName("tr");
+                for (i = 0; i < tr.length; i++) {
+                    td = tr[i].getElementsByTagName("td")[2];
+                    if (td) {
+                        txtValue = td.textContent || td.innerText;
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                            tr[i].style.display = "";
+                        } else {
+                            tr[i].style.display = "none";
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+
+
+
